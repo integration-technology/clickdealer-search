5061511d76e7f56685c576d9b8b12738cc41770b
owain <owain@intergration.technology>
2025-11-25 12:12:48 +0000

    Initial commit

---
+++ lib/clickdealer_search.ex
@@ -0,0 +1,18 @@
+defmodule ClickdealerSearch do
+  @moduledoc """
+  Documentation for `ClickdealerSearch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ClickdealerSearch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/clickdealer_search_test.exs
@@ -0,0 +1,8 @@
+defmodule ClickdealerSearchTest do
+  use ExUnit.Case
+  doctest ClickdealerSearch
+
+  test "greets the world" do
+    assert ClickdealerSearch.hello() == :world
+  end
+end
