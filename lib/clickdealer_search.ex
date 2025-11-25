7fc2b926cc8ea20778889accc96fc5acd711cdcb
owain <owain@intergration.technology>
2025-11-25 12:26:56 +0000

    search call success

--- lib/clickdealer_search.ex
+++ lib/clickdealer_search.ex
@@ -8,11 +8,48 @@ defmodule ClickdealerSearch do
 
   ## Examples
 
-      iex> ClickdealerSearch.hello()
+      iex> ClickdealerSearch.run()
       :world
 
   """
-  def hello do
-    :world
+  def run do
+    body =
+      %{
+        query: "",
+        filters: %{
+          all: [
+            %{manufacturer: "Volvo"},
+            %{range: "Xc90"}
+          ]
+        },
+        sort: [
+          %{"_score" => "desc"},
+          %{"price" => "desc"}
+        ],
+        page: %{size: 24, current: 1}
+      }
+      |> Jason.encode!()
+
+    headers = [
+      {"Content-Type", "application/json"},
+      {"Authorization", "Bearer search-jbcneo5evs3fed4wvgz1hx8n"}
+    ]
+
+    url =
+      "https://advanced-search-v2.clickdealer.co.uk/api/as/v1/engines/prod-3729-v1/search.json"
+
+    case HTTPoison.post(url, body, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: response_body}} ->
+        IO.puts("Success!")
+        IO.inspect(Jason.decode!(response_body))
+
+      {:ok, %HTTPoison.Response{status_code: code, body: response_body}} ->
+        IO.puts("HTTP error: #{code}")
+        IO.inspect(response_body)
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.puts("HTTPoison error:")
+        IO.inspect(reason)
+    end
   end
 end
